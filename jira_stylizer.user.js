6de46ae6fc634f7a32825d49e48adf3a0c5e3437
Iron-Wolf <Iron-Wolf@users.noreply.github.com>
2025-07-22 12:07:14 +0200

    jira_stylizer.user : big refactoring with a global config

--- jira_stylizer.user.js
+++ jira_stylizer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name            Jira's Supercharged Quantum-Powered Stylizer of Doom
-// @version         1.1.0
+// @version         1.1.1
 // @description     Bring your Jira experience to new levels and boost your productivity
 // @description:fr  Jira en tout pareil, mais c'est différent
 // @author          Iron-Wolf (https://github.com/Iron-Wolf)
@@ -10,7 +10,23 @@
 // ==/UserScript==
 
 
-// CSS Styles injected in the HEAD
+// ===============
+//      CONFIG
+// ===============
+const CONFIG = {
+    dayThreshold: 100,
+    matchers: {
+        waiting: 'Waiting',
+        toReview: 'A revoir',
+        resolved: 'Résolue'
+    },
+    debug: false
+};
+
+
+// ===============
+//      STYLES
+// ===============
 var css_wait = `
 .css_wait {
   position: absolute;
@@ -21,8 +37,7 @@ var css_wait = `
   z-index: 1;
   opacity: 0.5;
   rotate: -20deg;
-}
-`;
+}`;
 
 var css_rainbow = `
 .css_rainbow {
@@ -50,8 +65,7 @@ var css_rainbow = `
     0%{background-position:0% 82%}
     50%{background-position:100% 19%}
     100%{background-position:0% 82%}
-}
-`;
+}`;
 
 var css_shake = `
 .css_shake {
@@ -64,8 +78,7 @@ var css_shake = `
   50% { transform: translate(0, 0) rotate(0deg); }
   75% { transform: translate(0, 0) rotate(-2deg); }
   100% { transform: translate(0, 0) rotate(0deg); }
-}
-`;
+}`;
 
 var css_hover = `
 .hover-in{
@@ -73,58 +86,86 @@ var css_hover = `
 }
 .hover-out{
     transition: .3s ease-out;
+}`;
+
+
+// ===============
+//    UTILITIES
+// ===============
+
+const log = (...args) =>
+    CONFIG.debug
+    && console.log('[Jira Customizer]', ...args);
+
+// styles : list of text block to add in the HEAD of the page
+function injectStyles(...styles) {
+    styles.forEach(styleText => {
+        const el = document.createElement('style');
+        el.textContent = styleText;
+        document.head.appendChild(el);
+    });
+};
+
+function insertChildAtIndex(parent, child, index = 0) {
+    if (index >= parent.children.length) {
+        parent.appendChild(child);
+    } else {
+        parent.insertBefore(child, parent.children[index]);
+    }
 }
-`;
+
+function wrapAndGetParent(container) {
+    const wrapper = document.createElement("div");
+    // add wrapper before the container
+    container.parentNode.insertBefore(wrapper, container);
+    // add the container inside it
+    wrapper.appendChild(container);
+    return wrapper;
+}
 
 
 // ===============
 //      MAIN
 // ===============
 (function() {
-    function addCssInHead(text) {
-        var cssElement = document.createElement("style");
-        cssElement.textContent = text;
-        document.head.appendChild(cssElement);
-    }
+    injectStyles(css_wait,
+                 css_rainbow,
+                 css_shake,
+                 css_hover);
 
-    addCssInHead(css_wait);
-    addCssInHead(css_rainbow);
-    addCssInHead(css_shake);
-    addCssInHead(css_hover);
+    // one liner to observe the page, and call the "check" method
+    new MutationObserver(check).observe(document, {childList: true, subtree: true});
 })();
 
 
-// Create a global function "insertChildAtIndex" for the entire code.
-// Can be dangerous : https://stackoverflow.com/a/507157
-Element.prototype.insertChildAtIndex = function(child, index) {
-    if (!index) index = 0;
-    if (index >= this.children.length) {
-        this.appendChild(child);
-    } else {
-        this.insertBefore(child, this.children[index]);
-    }
-}
+function createShine() {
+    // create "shine" effect, used with the 3D movement
+    let shine;
+    shine = document.createElement("div");
+    shine.classList.add("shine");
 
-function wrapAndGetParent(container) {
-  const wrapper = document.createElement("div");
-  // add wrapper before the container
-  container.parentNode.insertBefore(wrapper, container);
-  // add the container inside it
-  wrapper.appendChild(container);
-  return wrapper;
+    Object.assign(shine.style, {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        transform: "translateZ(1px)",
+        zIndex: 9,
+        pointerEvents: "none",
+    });
+    return shine;
 }
 
 // addapted from : https://codepen.io/MuriloFeranti/pen/JrgRJo?editors=1111
-function apply3DEffect(container) {
+function apply3d(container) {
     if (!container || !(container instanceof HTMLElement)) {
         console.warn("[apply3DEffect] invalid param", container);
         return;
     }
 
     // do nothing if already set on the element
-    if (container.dataset.hover3dApplied === "true") {
-        return;
-    }
+    if (container.dataset.hover3dApplied === "true") return;
     container.dataset.hover3dApplied = "true";
 
     const settings = {
@@ -133,35 +174,19 @@ function apply3DEffect(container) {
         invert: true,
         shine: true,
         hoverInClass: "hover-in",
-        hoverOutClass: "hover-out",
-        hoverClass: "hover-3d"
+        hoverOutClass: "hover-out"
     };
 
-    // lazzy "hack" to add the container inside a root DIV
-    const parent = wrapAndGetParent(container);
+    // small "hack" to add the container inside a root DIV
+    const outer = wrapAndGetParent(container);
     const inner = container;
 
     // need carrefull setup to have a true perspective
-    parent.style.perspective = settings.perspective + "px"; // perspective ONLY on the parent
+    outer.style.perspective = settings.perspective + "px"; // add perspective ONLY to the outer div
     inner.style.transformStyle = "preserve-3d";
 
     // create "shine" effect
-    let shine;
-    if (settings.shine) {
-        shine = document.createElement("div");
-        shine.classList.add("shine");
-
-        Object.assign(shine.style, {
-            position: "absolute",
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            transform: "translateZ(1px)",
-            zIndex: 9,
-            pointerEvents: "none",
-        });
-    }
+    const shine = createShine()
 
     function hoverIn() {
         if (settings.shine) {
@@ -173,6 +198,7 @@ function apply3DEffect(container) {
             inner.classList.remove(settings.hoverInClass);
         }, 1000);
     }
+    container.addEventListener("mouseenter", hoverIn);
 
     function hoverMove(e) {
         const rect = inner.getBoundingClientRect();
@@ -207,69 +233,68 @@ function apply3DEffect(container) {
                                       rgba(010,010,010,${endOpacity}) 80%)`;
         }
     }
+    container.addEventListener("mousemove", hoverMove);
 
     function hoverOut() {
+        // remove the effect
         if (settings.shine) {
             inner.removeChild(shine);
         }
 
-        inner.classList.add(settings.hoverOutClass, settings.hoverClass);
+        // reset positions
         inner.style.transform = `rotateX(0deg) rotateY(0deg)`;
 
+        inner.classList.add(settings.hoverOutClass);
         setTimeout(() => {
-            inner.classList.remove(settings.hoverOutClass, settings.hoverClass);
+            inner.classList.remove(settings.hoverOutClass);
         }, 1000);
     }
-
-    // Événements
-    container.addEventListener("mouseenter", hoverIn);
-    container.addEventListener("mousemove", hoverMove);
     container.addEventListener("mouseleave", hoverOut);
 }
 
 
-// one liner to observe the page, and call the "check" method
-(new MutationObserver(check)).observe(document, {childList: true, subtree: true});
+function addEffects(nodeItem) {
+    apply3d(nodeItem);
+
+    // Jira Card that have a "Days" component at the bottom
+    const regexDays = /(ghx-days-)(\d+)/g;
+    var nodeItemClass = nodeItem.classList;
+    nodeItemClass.forEach(function(className) {
+        // use "matchAll" to retrieve all groups of the regex
+        const numberOfDays = Array.from(className.matchAll(regexDays), m => m[2])[0];
+        if (numberOfDays > CONFIG.dayThreshold){
+            nodeItem.classList.add("css_shake");
+        }
+    });
+
+    // Jira Card that have extra fields (class = "ghx-extra-field-content")
+    var extraFieldNodeList = nodeItem.querySelectorAll("span.ghx-extra-field-content");
+    extraFieldNodeList.forEach(function(eNodeItem) {
+        if(eNodeItem.textContent.startsWith("Waiting")) {
+            nodeItem.style.background = "lightgray";
+            // check if our DIV is already there (otherwize, it will hug the CPU by creating infinite DOM elements)
+            if (nodeItem.querySelectorAll("div.css_wait").length <= 0) {
+                var div = document.createElement("div");
+                div.classList.add("css_wait");
+                div.innerHTML = "WAIT";
+                insertChildAtIndex(nodeItem, div, 0);
+            }
+        }
+        else if(eNodeItem.textContent.startsWith("A revoir")) {
+            nodeItem.style.background = "lightpink";
+        }
+        else if(eNodeItem.textContent.startsWith("Résolue")) {
+            nodeItem.classList.add("css_rainbow");
+        }
+    });
+}
+
 
 function check(changes, observer) {
+    //observer.disconnect(); // Panic Button if we want to debug the script
+    log("Observer hit");
+
     // search node with the class "ghx-issue" and a specific child
-    var nodeList = document.querySelectorAll("div.ghx-issue:has(span.ghx-extra-field-content)");
-    const regexDays = /(ghx-days-)(\d+)/g;
-
-    if(nodeList.length > 0) {
-        //observer.disconnect(); // Panic Button if we want to debug the script
-        nodeList.forEach(function(nodeItem) {
-            apply3DEffect(nodeItem);
-            // Jira Card that have a "Days" component at the bottom
-            var nodeItemClass = nodeItem.classList;
-            nodeItemClass.forEach(function(className) {
-                // use "matchAll" to retrieve all groups of the regex
-                const numberOfDays = Array.from(className.matchAll(regexDays), m => m[2])[0];
-                if (numberOfDays > 100){
-                    nodeItem.classList.add("css_shake");
-                }
-            });
-
-            // Jira Card that have extra fields (class = "ghx-extra-field-content")
-            var extraFieldNodeList = nodeItem.querySelectorAll("span.ghx-extra-field-content");
-            extraFieldNodeList.forEach(function(eNodeItem) {
-                if(eNodeItem.textContent.startsWith("Waiting")) {
-                    nodeItem.style.background = "lightgray";
-                    // check if our DIV is already there (otherwize, it will hug the CPU by creating infinite DOM elements)
-                    if (nodeItem.querySelectorAll("div.css_wait").length <= 0) {
-                        var div = document.createElement("div");
-                        div.classList.add("css_wait");
-                        div.innerHTML = "WAIT";
-                        nodeItem.insertChildAtIndex(div, 0);
-                    }
-                }
-                else if(eNodeItem.textContent.startsWith("A revoir")) {
-                    nodeItem.style.background = "lightpink";
-                }
-                else if(eNodeItem.textContent.startsWith("Résolue")) {
-                    nodeItem.classList.add("css_rainbow");
-                }
-            });
-        });
-    }
+    const nodeList = document.querySelectorAll('div.ghx-issue:has(span.ghx-extra-field-content)');
+    nodeList.forEach(addEffects);
 }
